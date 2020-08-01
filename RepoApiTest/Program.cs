44d7e7b81e0facc0e947f0da32b5dd062bdf0970
Stefan van der Velden <stefan.vdv@virtualspectator.com>
2020-08-01 13:16:28 +1000

    test: manually test the behaviour of the load function

---
+++ RepoApiTest/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using RepositoryApi;
+
+namespace RepoApiTest
+{
+    class Program
+    {
+        static void Main(string[] _)
+        {
+            using var api = RepoApi.Load("https://github.com/MinecraftPhi/mc-tools.git", "mc-tools", "dev");
+        }
+    }
+}

ef24098745e33ef84750d0c175a6b17e3f24a8e0
Stefan van der Velden <stefan.vdv@virtualspectator.com>
2020-08-01 13:08:28 +1000

    feat: add helper for retrying an action in case of an exception

---
+++ RepoApi/Helpers/ActionHelpers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepositoryApi.Helpers
+{
+    public static class ActionHelpers
+    {
+        public static void RetryOnException(Action action, int maxTries = 10)
+        {
+            for (int i = 0; i < maxTries; i++)
+            {
+                try
+                {
+                    action();
+                    return;
+                }
+                catch { }
+            }
+        }
+    }
+}
